1d21956db2127ae36bb83c146368882e775e22ef
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-05-28 10:04:23 +0100

    Create services project
    For Business Logic

---
+++ LojaQuadrinhos.Services/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LojaQuadrinhos.Services
+{
+    public class Class1
+    {
+        //requested, check "product" avaiability before buying it
+        //to buy check if the users exists
+    }
+}
