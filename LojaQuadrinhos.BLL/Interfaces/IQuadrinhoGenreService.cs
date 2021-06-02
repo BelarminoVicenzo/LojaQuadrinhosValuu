a6085ba91792e9d7a96081f313cda4a894760d12
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-02 16:09:04 +0100

    created  IQuadrinhoGenreService<T>

---
+++ LojaQuadrinhos.BLL/Interfaces/IQuadrinhoGenreService.cs
@@ -0,0 +1,16 @@
+﻿using LojaQuadrinhos.Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.BLL.Interfaces
+{
+    public interface IQuadrinhoGenreService<T>
+    {
+        Task<T> GetGenreAsync(int id);
+        Task<int> CreateGenreAsync(QuadrinhoGenre entity);
+        Task<List<QuadrinhoGenre>> GetAllGenreAsync();
+        Task<int> UpdateGenreAsync(QuadrinhoGenre entity);
+        Task<int> DeleteGenreAsync(QuadrinhoGenre entity);
+    }
+}
