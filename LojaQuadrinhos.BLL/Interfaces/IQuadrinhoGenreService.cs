47cad775cd26ead3fb6aefec4ffb21725d4eb1cb
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-02 16:12:58 +0100

    refactored IQuadrinhoGenreServices methods parameter type

--- LojaQuadrinhos.BLL/Interfaces/IQuadrinhoGenreService.cs
+++ LojaQuadrinhos.BLL/Interfaces/IQuadrinhoGenreService.cs
@@ -8,9 +8,9 @@ namespace LojaQuadrinhos.BLL.Interfaces
     public interface IQuadrinhoGenreService<T>
     {
         Task<T> GetGenreAsync(int id);
-        Task<int> CreateGenreAsync(QuadrinhoGenre entity);
-        Task<List<QuadrinhoGenre>> GetAllGenreAsync();
-        Task<int> UpdateGenreAsync(QuadrinhoGenre entity);
-        Task<int> DeleteGenreAsync(QuadrinhoGenre entity);
+        Task<int> CreateGenreAsync(T entity);
+        Task<List<T>> GetAllGenreAsync();
+        Task<int> UpdateGenreAsync(T entity);
+        Task<int> DeleteGenreAsync(T entity);
     }
 }
