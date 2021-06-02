b0b0eaf70b1373444dff8418e71072da5d90d9f7
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-02 15:55:50 +0100

    implemented IQuadrinhoGenreRepository interface on QuadrinhoGenreRepository

---
+++ LojaQuadrinhos.DataAccess/Repository/QuadrinhoGenreRepository.cs
@@ -0,0 +1,51 @@
+﻿using LojaQuadrinhos.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.DataAccess.Repository
+{
+    public class QuadrinhoGenreRepository : IQuadrinhoGenreRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        
+        public QuadrinhoGenreRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Create(QuadrinhoGenre entity)
+        {
+            _context.QuadrinhoGenre.Add(entity);
+           return await  _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Update(QuadrinhoGenre entity)
+        {
+
+            _context.Update(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(QuadrinhoGenre entity)
+        {
+            _context.Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public  async Task<QuadrinhoGenre> Get(object id)
+        {
+            return await _context.QuadrinhoGenre.FindAsync(id);
+        }
+
+        public async Task<List<QuadrinhoGenre>> GetAll()
+        {
+            return await _context.QuadrinhoGenre.ToListAsync();
+        }
+
+    }
+}
