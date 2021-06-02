0bd8f2a809a06b0e0abe4a8fa408955ace8b3c44
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-02 15:25:21 +0100

    Added IoC DI library project
    is targeting aspnet core 5

---
+++ LojaQuadrinhosValuu.IoC/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using LojaQuadrinhos.BLL;
+using LojaQuadrinhos.DataAccess;
+using LojaQuadrinhos.DataAccess.Repository;
+using LojaQuadrinhos.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LojaQuadrinhosValuu.IoC
+{
+    public class DependencyInjection
+    {
+        private readonly IConfiguration Configuration;
+        public DependencyInjection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void InjectDependencies(IServiceCollection services)
+        {
+            /* Entity Framework connString */
+           
+
+            services.AddScoped<IQuadrinhoGenreService<QuadrinhoGenre>, QuadrinhoGenreService>();
+            services.AddScoped<IQuadrinhoGenreRepository, QuadrinhoGenreRepository>();
+
+          
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LojaQuadrinhos")));
+        }
+    }
+
+
+
+
+
+
+
+}
