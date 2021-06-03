402cf758a58fa4e4039c51c020226ebc961b3235
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-03 10:05:15 +0100

    created Employee service and interface

---
+++ LojaQuadrinhos.BLL/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.BLL.Interfaces
+{
+    public interface IEmployeeService<T>
+    {
+        Task<T> GetEmployeeAsync(int id);
+        Task<int> CreateEmployeeAsync(T entity);
+        Task<List<T>> GetAllEmployeeAsync();
+        Task<int> UpdateEmployeeAsync(T entity);
+        Task<int> DeleteEmployeeAsync(T entity);
+    }
+}
