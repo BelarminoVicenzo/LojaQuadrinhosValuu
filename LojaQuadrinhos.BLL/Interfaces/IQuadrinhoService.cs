7868c35691f6960aff5352253d666a593ed97ac9
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-03 17:27:56 +0100

    created QuadrinhoService class and interface

---
+++ LojaQuadrinhos.BLL/Interfaces/IQuadrinhoService.cs
@@ -0,0 +1,17 @@
+﻿using LojaQuadrinhos.Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.BLL.Interfaces
+{
+    public interface IQuadrinhoService
+    {
+        Task<Quadrinho> GetQuadrinhoAsync(int id);
+        Task<int> CreateQuadrinhoAsync(Quadrinho quadrinho);
+        Task<List<Quadrinho>> GetAllQuadrinhosAsync();
+        Task<int> UpdateQuadrinhoAsync(Quadrinho quadrinho);
+        Task<int> DeleteQuadrinhoAsync(Quadrinho quadrinho);
+      
+    }
+}
---
+++ LojaQuadrinhos.BLL/Services/QuadrinhoService.cs
@@ -0,0 +1,48 @@
+﻿using LojaQuadrinhos.BLL.Interfaces;
+using LojaQuadrinhos.DataAccess.Repository;
+using LojaQuadrinhos.Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.BLL.Service
+{
+    public class QuadrinhoService : IQuadrinhoService
+    {
+
+        IQuadrinhoRepository _repo;
+        public QuadrinhoService(IQuadrinhoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<Quadrinho>> GetAllQuadrinhosAsync()
+        {
+            return await _repo.GetAll();
+        }
+
+        public async Task<Quadrinho> GetQuadrinhoAsync(int id)
+        {
+            var quad = await _repo.Get(id);
+            if (quad != null) return quad;
+            else return new Quadrinho();
+        }
+
+
+        public async Task<int> CreateQuadrinhoAsync(Quadrinho quadrinho)
+        {
+                return await _repo.Create(quadrinho);
+        }
+
+        public async Task<int> UpdateQuadrinhoAsync(Quadrinho quadrinho)
+        {
+            return await _repo.Update(quadrinho);
+        }
+
+        public async Task<int> DeleteQuadrinhoAsync(Quadrinho quadrinho)
+        {
+            return await _repo.Delete(quadrinho);
+        }
+
+    }
+}
