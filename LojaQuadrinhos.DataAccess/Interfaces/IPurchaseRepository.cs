ff542b39c03b700f2fc2371bedaaebf7233b7dd0
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-03 15:06:54 +0100

    create repository class and interface

---
+++ LojaQuadrinhos.DataAccess/Interfaces/IPurchaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using LojaQuadrinhos.DataAccess.Interfaces;
+using LojaQuadrinhos.Models;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.DataAccess.Repository
+{
+    public interface IPurchaseRepository:IGenericRepository<Purchase>
+    {
+        Task<List<Purchase>> GetFromCustomer(object customerId);
+    }
+}
---
+++ LojaQuadrinhos.DataAccess/Repository/PurchaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using LojaQuadrinhos.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhos.DataAccess.Repository
+{
+    public class PurchaseRepository : IPurchaseRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public PurchaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Create(Purchase entity)
+        {
+            _context.Purchase.Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<Purchase> Get(object id)
+        {
+            return await _context.Purchase.FindAsync(id);
+        }
+
+        public async Task<List<Purchase>> GetAll()
+        {
+            return await _context.Purchase.ToListAsync();
+        }
+        
+        public async Task<List<Purchase>> GetFromCustomer(object customerId)
+        {
+            return await _context.Purchase.Where(p => p.Id == (int)customerId).ToListAsync();
+        }
+        
+    }
+}
