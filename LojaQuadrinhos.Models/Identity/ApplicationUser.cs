062e54a8daa0a9ffba9fbe0ca1bf7880efc40442
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-08 08:51:08 +0100

    removed ApplicationUser from DataAccess and Added to Models project

---
+++ LojaQuadrinhos.Models/Identity/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LojaQuadrinhos.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+     
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
+        
+        public int UserTypeId { get; set; }
+
+        [ForeignKey("UserTypeId")]
+        public virtual UserType UserType { get; set; }
+
+    }
+
+}
