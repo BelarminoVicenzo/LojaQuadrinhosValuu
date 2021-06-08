b2864a57bc3c8e4c4aba063582b4eeb772b3647f
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-08 08:49:20 +0100

    correction migration
    didn't change nothing

--- LojaQuadrinhos.DataAccess/Migrations/20210608074554_test.Designer.cs
+++ LojaQuadrinhos.DataAccess/Migrations/20210608074554_test.Designer.cs
@@ -4,14 +4,16 @@ using LojaQuadrinhos.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LojaQuadrinhos.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210608074554_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LojaQuadrinhos.DataAccess/Migrations/20210608074554_test.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaQuadrinhos.DataAccess.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchase_AspNetUsers_Userid",
+                table: "Purchase");
+
+            migrationBuilder.RenameColumn(
+                name: "Userid",
+                table: "Purchase",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Purchase_Userid",
+                table: "Purchase",
+                newName: "IX_Purchase_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchase_AspNetUsers_UserId",
+                table: "Purchase",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchase_AspNetUsers_UserId",
+                table: "Purchase");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Purchase",
+                newName: "Userid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Purchase_UserId",
+                table: "Purchase",
+                newName: "IX_Purchase_Userid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchase_AspNetUsers_Userid",
+                table: "Purchase",
+                column: "Userid",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
