fa322207309d5c00eacb46b5da8ea06e7145509f
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-08 19:48:28 +0100

    created usercontroller and views

---
+++ LojaQuadrinhosWeb/Controllers/UsersController.cs
@@ -0,0 +1,100 @@
+﻿using LojaQuadrinhos.BLL.Interfaces;
+using LojaQuadrinhos.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaQuadrinhosWeb.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        IUserService _userService;
+        IUserTypeService _userType;
+        public UsersController(IUserService userService, IUserTypeService userType)
+        {
+            _userService = userService;
+            _userType = userType;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _userService.GetUsersAsync()) ;
+        }
+
+       
+        public async Task<IActionResult> CreateAsync()
+        {
+            var userTypes = from u in await _userType.GetAllUserTypeAsync() where u.Type!="Employee" select u;
+            ViewBag.UserType = new SelectList(userTypes, "Id", "Type") ;
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(ApplicationUser user)
+        {
+            await _userService.CreateUserAsync(user);
+            return RedirectToAction("Index");
+        }
+
+      
+        public async Task<IActionResult> UpdateAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var state = await _userService.GetUserAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return View(state);
+        }
+
+     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateAsync([Bind("Id,Name")] ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.UpdateUserAsync(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+   
+        public async Task<IActionResult> DeleteAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var state = await _userService.GetUserAsync(id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return View(state);
+        }
+
+      
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedAsync(string id)
+        {
+            await _userService.DeleteUserAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
