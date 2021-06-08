562e57ee5ff6e8ac0d46eb799ccb2f129ee21ff7
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-08 19:48:04 +0100

    created LoginModel class

---
+++ LojaQuadrinhos.BLL/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaQuadrinhos.BLL.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+}
