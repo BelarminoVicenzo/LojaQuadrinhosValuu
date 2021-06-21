9feb586e7c82dd671c5dbada7f9a02bcec0f002d
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-06-21 12:28:48 +0100

    refactor interface  IUserSevice
    Added methods:
    AddToRole
    GetUserWithoutSensetiveInfo
    RemoveFromRole
    GetRolesFromUser
    GetUserByUserNameAsync

--- LojaQuadrinhos.BLL/Interfaces/IUserService.cs
+++ LojaQuadrinhos.BLL/Interfaces/IUserService.cs
@@ -9,10 +9,15 @@ namespace LojaQuadrinhos.BLL.Interfaces
     public interface IUserService
     {
         Task<ApplicationUser> GetUserAsync(string id);
+        List<ApplicationUser> GetUserWithoutSensetiveInfo();
         Task<IdentityResult> CreateUserAsync(ApplicationUser entity);
         Task<List<ApplicationUser>> GetUsersAsync();
         Task<IdentityResult> UpdateUserAsync(ApplicationUser entity);
         Task<IdentityResult> DeleteUserAsync(string id);
+        Task<IdentityResult> AddToRole(string userId, string roleName);
+        Task<IdentityResult> RemoveFromRole(string userId, string roleName);
+        Task<List<string>> GetRolesFromUser(string userId);
+        Task<ApplicationUser> GetUserByUserNameAsync(string userName);
 
     }
 }
